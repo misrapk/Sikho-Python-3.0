beee8493e12d7d99434738e2faee526df3416989
Peeyush Kant Misra <pkmisra1999@gmail.com>
2023-12-01 09:17:11 +0530

    1st code
    You can visit the URL to watch the tutorial. https://www.youtube.com/playlist?list=PLMULAM6iDn09M0VLFOg62MWewnqtHlCbO

---
+++ 01Basics/01helloworld.py
@@ -0,0 +1,35 @@
+# print("Hello world")
+# print("My name is")
+# print("Peeyush")
+
+
+# print("Hello" + "India")
+# print("Hello" + " " + "India")
+# print("Hello " + "India")
+# print("Hello" + " India")
+
+# print("WHat is your name?")
+
+
+# input("WHat is your name?")
+# input("what is your age?")
+
+# print(len(input("WHat is your name?")))
+# input("WHat is your name?")
+
+name1 = "peeyush misra"
+# name2 = "ram"
+name2 = "ram"
+
+print(id(name1))
+print(id(name2))
+
+# (A-Z)(a-z)(0-9)(_)
+
+Name = "p"
+name = "s"
+
+
+
+# schoolName #camelCasing
+# school_name #snake case

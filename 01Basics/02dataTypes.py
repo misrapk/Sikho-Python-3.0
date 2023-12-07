053cce81ba36d8c2db9cb8bd9594dd3e606d0bbb
Peeyush Kant Misra <pkmisra1999@gmail.com>
2023-12-07 10:30:15 +0530

    Data Type and boolean data type code

---
+++ 01Basics/02dataTypes.py
@@ -0,0 +1,52 @@
+# String, Numbers --> float, large, int
+
+#TODO: STRING
+
+# name = "spdider man"     #Spider Man
+# superHero = "THOR"
+# print(name.title())
+# print(name.upper())
+# print(superHero.lower())
+# print(superHero.title())
+
+
+
+# print("enter your details!\n")
+# firstName = input("Enter First Name: ")
+# lastName = input("Enter last Name: ")
+
+
+# fullName = f"{firstName} is {lastName}"
+# print(firstName, lastName)
+
+
+# prefix = 'Py'
+
+# print(f"{prefix}thon")
+
+
+#TODO: NUMBER - int, float
+age = 21
+print(3**2)
+print(3/2)
+
+equation = 2*3+1*4
+print(equation)
+
+
+#1,26,24,334
+cash = 1_26_24_334
+print(cash)
+
+
+cash2 = 0.334
+print(cash2)
+cash3 = int(cash2)
+print(cash3)
+
+
+age = int(input("ENter your age in number "))  # int("21") --> 21
+# print(type(int(age)))
+# print(int(age) + 1) 
+print(f"Your age after one year will be {age +1}")
+
---
+++ 01Basics/03BooleanDataType.py
@@ -0,0 +1,59 @@
+#boolean data type --> 0 or 1, False or True
+"""
+     ==  --> Equal to 
+     != --> Not Equal to
+     a<b --> Less than
+     > --> greater than
+     <= Lessthan equal to
+     >= Great than or equal to
+     
+     
+     Binary Boolean Operators
+     
+     and --> both should be True for true result
+     or --> atleast one should be True for true result.
+     
+     Unary B O
+     not -->
+     
+     
+     T and T == T           T or T == T
+     T and F == F           T or F == T
+     F and T == F           F or T == T 
+     F and F == F           F or F == F
+     
+     not True == False 
+     not False == True
+"""
+
+# isRaining = True  #True
+
+
+# a = 42
+# b = 42
+
+# result = a<=b
+
+# print(type(isRaining))
+# print(result)
+
+
+
+# print(True and False)
+# print(True or False)
+# print(not True)
+
+
+a = 5
+b = 7
+c = 8
+d = 6
+
+result1 = a<b  #True 
+result2 = b<c  #True
+result3 = b<d  #False
+
+# print(result1)
+# print(result2)
+
+print(result1 or result3)
